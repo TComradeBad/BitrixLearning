057c4412dfaed6c84477000b3fc4cc558a39a428
TBadamshin <t.badamshin@mediasoft.team>
2020-03-06 18:50:40 +0400

    init

--- vendor/matiaspub/bx-api-docs/modules/crm/classes/mysql/crm_contact.php
+++
@@ -1,8 +0,0 @@
-<?php
-IncludeModuleLangFile(__FILE__);
-
-class CCrmContact extends CAllCrmContact
-{
-    const TABLE_NAME = 'b_crm_contact';
-    const DB_TYPE = 'MYSQL';
-}
--- vendor/matiaspub/bx-api-docs/modules/sale/lib/businessvalueproviderinterface.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Bitrix\Sale;
-
-interface IBusinessValueProvider
-{
-	public function getPersonTypeId();
-	static public function getBusinessValueProviderInstance($mapping);
-}
